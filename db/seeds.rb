a1a54b91d50ab6c4f22a59b466f1455882b45ac8
JackBruzan <jackbruzan@gmail.com>
2017-07-16 17:20:26 -0500

    demo seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Routes.create(routeid: '22', routename: 'Clark')
